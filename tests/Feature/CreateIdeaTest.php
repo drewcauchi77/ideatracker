7fe120ed6fbbb801763ef53b185c00a6071b3a39
Andrew Cauchi <drewcauchi@gmail.com>
2025-06-25 23:55:27 +0200

    Creating tests for ideas

--- tests/Feature/CreateIdeaTest.php
+++ tests/Feature/CreateIdeaTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Livewire\CreateIdea;
+use App\Models\Status;
 use App\Models\User;
+use App\Models\Category;
+use Livewire\Livewire;
 
 test('create idea form does not show when logged out', function () {
     $response = $this->get(route('idea.index'));
@@ -20,5 +24,75 @@ test('create idea form does show when logged in', function () {
 
 
 test('main page contains create idea component', function () {
-    $this->actingAs(User::factory()->create())->get(route('idea.index'))->assertSee('create-idea');
+    $this->actingAs(User::factory()->create())
+        ->get(route('idea.index'))
+        ->assertSeeLivewire('create-idea');
+});
+
+test('create idea form validation works', function () {
+    Livewire::actingAs(User::factory()->create())
+        ->test(CreateIdea::class)
+        ->set('title', '')
+        ->set('description', '')
+        ->set('category', '')
+        ->call('createIdea')
+        ->assertHasErrors(['title', 'description', 'category'])
+        ->assertSee('The title field is required.');
+});
+
+
+test('creating an idea works correctly', function () {
+    $user = User::factory()->create();
+    $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+    $statusOpen = Status::factory()->create(['name' => 'Open', 'color' => 'green']);
+
+    Livewire::actingAs($user)
+        ->test(CreateIdea::class)
+        ->set('title', 'Title Name')
+        ->set('description', 'This is a description')
+        ->set('category', $categoryOne->id)
+        ->call('createIdea')
+        ->assertRedirect(route('idea.index'));
+
+    $response = $this->actingAs($user)->get(route('idea.index'));
+    $response->assertStatus(200);
+    $response->assertSee('Title Name');
+    $response->assertSee('This is a description');
+
+    $this->assertDatabaseHas('ideas', [
+        'title' => 'Title Name',
+    ]);
+});
+
+test('creating two ideas with same title still works with different slugs', function () {
+    $user = User::factory()->create();
+    $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+
+    $statusOpen = Status::factory()->create(['name' => 'Open', 'color' => 'green']);
+
+    Livewire::actingAs($user)
+        ->test(CreateIdea::class)
+        ->set('title', 'Title Name')
+        ->set('description', 'This is a description')
+        ->set('category', $categoryOne->id)
+        ->call('createIdea')
+        ->assertRedirect(route('idea.index'));
+
+    $this->assertDatabaseHas('ideas', [
+        'title' => 'Title Name',
+        'slug' => 'title-name',
+    ]);
+
+    Livewire::actingAs($user)
+        ->test(CreateIdea::class)
+        ->set('title', 'Title Name')
+        ->set('description', 'This is a description')
+        ->set('category', $categoryOne->id)
+        ->call('createIdea')
+        ->assertRedirect(route('idea.index'));
+
+    $this->assertDatabaseHas('ideas', [
+        'title' => 'Title Name',
+        'slug' => 'title-name-2',
+    ]);
 });
