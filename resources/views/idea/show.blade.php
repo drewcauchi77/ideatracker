d23816ada2d407b6f32046073deb62cc1b40b42d
Andrew <drewcauchi@gmail.com>
2025-06-26 08:26:56 +0200

    Voting

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -1,9 +1,4 @@
 <a href="{{ route('idea.index') }}">Go Back</a>
 
 <h1>Idea</h1>
-<img src="{{ $idea->user->avatar }}" alt="Avatar" />
-<span>{{ $idea->title }}</span><br>
-<p>{{ $idea->description }}</p>
-<span>Category: {{ $idea->category->name }}</span><br>
-<span>Status: <strong style="color: {{ $idea->status->color }};">{{ $idea->status->name }}</strong></span><br>
-<span>{{ $idea->user->name }}</span><br>
+<livewire:idea-show :idea="$idea" />
